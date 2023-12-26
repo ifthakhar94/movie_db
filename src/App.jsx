7babbe7cab6f7c616b5a40eabf8c95b21f5d5b7c
ifthakhar <imahmud@idare.io>
2023-12-26 17:50:46 +0600

    API stracture ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 
 import './App.css'
+import { fetchDataFromAPI } from './utils/api'
 
 function App() {
 
+  const apiTesting = () =>{
+    fetchDataFromAPI("/movie/popular".then((res)=>{console.log(res)}))
+  }
   return (
     <>
       <h2>App</h2>
